0be68816581bf29609f1a679ea6b0aac02244f40
mengyunxie <my.xiemy@gmail.com>
2025-07-31 10:29:17 -0700

    create a pipeline

--- codePipelineWithCDK/lib/code_pipeline_with_cdk-stack.ts
+++ codePipelineWithCDK/lib/code_pipeline_with_cdk-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +7,20 @@ export class CodePipelineWithCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'TestPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('mengyunxie/aws-awesome', 'main'),
+        commands: [
+          // Move into the subdirectory
+          'cd codePipelineWithCDK',
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CodePipelineWithCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+          // Install dependencies and synth
+          'npm ci',
+          'npm run build',
+          'npx cdk synth'
+        ]
+      }),
+    });
   }
 }
